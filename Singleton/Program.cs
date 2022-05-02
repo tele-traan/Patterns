fcd627a2a3cba76b248b3cfd3c733e0ffc07be79
Timur <timurturovets@mail.ru>
2022-05-02 18:43:09 +0300

    Паттерн "Одиночка"

---
+++ Singleton/Program.cs
@@ -0,0 +1,14 @@
+﻿using Singleton;
+
+SingletonObject.Instance = new SingletonObject();
+var object1 = SingletonObject.Instance;
+
+SingletonObject.Instance = new SingletonObject();
+var object2 = SingletonObject.Instance;
+
+Console.WriteLine(
+    object1 == object2
+    ? "object1 и object2 содержат ссылки на одну и ту же сущность объекта-одиночки"
+    : "object1 и object2 содержат ссылки на разные сущности объекта-одиночки"
+);
+//Выводит: object1 и object2 содержат ссылки на одну и ту же сущность объекта-одиночки
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿namespace Singleton;
+
+public class SingletonObject
+{
+    private static SingletonObject? _instance;
+
+    public static SingletonObject? Instance
+    {
+        get
+        {
+            Console.WriteLine("Возврат объекта-одиночки");
+            return _instance;
+        }
+        set
+        {
+            Console.WriteLine("Попытка установить значение объекту-одиночке");
+            if (_instance is not null)
+            {
+                Console.WriteLine("Объект-одиночка уже содержит значение; новое значение не было установлено");
+                return;
+            }
+            Console.WriteLine("Объект-одиночка не содержит значения; новое значение установлено");
+            _instance = value;
+        }
+    }
+}
